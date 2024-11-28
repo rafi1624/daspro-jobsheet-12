ea5083616a27c03f02b14c55362d7f638585178b
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-11-28 08:24:20 +0700

    Modifikasi Kode Program Percobaan 2 Pertanyaan 2

--- Percobaan2Absen23.java
+++ Percobaan2Absen23.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 
 public class Percobaan2Absen23 {
-    static Scanner sc = new Scanner(System.in);
-    static int bilangan,pangkat;
+    static Scanner sc = new Scanner(System.in); 
+    static int bilangan, pangkat;
 
-    static int hitungPangkat ( int x, int y){
+    static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            System.out.print("x1"); 
+            return 1;
         } else {
-            return (x * hitungPangkat(x, y - 1));
+            System.out.print(x); 
+            if (y > 1) {
+                System.out.print("x");
+            }
+            return x * hitungPangkat(x, y - 1);
         }
-    } 
+    }
+
     public static void main(String[] args) {
         System.out.print("Bilangan Yang Dihitung: ");
-        bilangan = sc.nextInt();
+        bilangan = sc.nextInt(); 
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
-        
-        System.out.println(hitungPangkat(bilangan,pangkat));
-    } 
+
+        System.out.print("Hasil perhitungan: ");
+        int hasil = hitungPangkat(bilangan, pangkat); 
+        System.out.println(" = " + hasil); 
+    }
 }
